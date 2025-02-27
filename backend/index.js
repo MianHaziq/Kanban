090c33a83fb5fa48ade51acef704190d8d71e82b
l1f21bsse0424 <l1f21bsse0424@ucp.edu.pk>
2025-02-28 01:22:53 +0500

    authentication completed

--- backend/index.js
+++ backend/index.js
@@ -10,20 +10,13 @@ const app=express();
 app.use(express.json());
 
 
-mongoose.connect("mongodb://localhost:27017/Kanban", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch(err => {
-    console.error("Error connecting to MongoDB:", err);
-});
+mongoose.connect("mongodb://localhost:27017/Kanban")
+
 
 
 
 
 app.use('/user', user_router);
-
 
 const PORT = 3002;
 app.listen(PORT, () => {
